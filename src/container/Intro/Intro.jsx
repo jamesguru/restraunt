7ae99137005ac75834955b43c0989a060c6561e4
powerscript <jameskagunga2022@gmail.com>
2022-02-16 15:27:24 +0300

    working on intro part

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -2,10 +2,57 @@ import React from 'react';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
+
+
+import {meal} from '../../constants';
+
+const Intro = () => {
+
+
+  const videoRef = React.useRef();
+
+  const [playVideo, setPlayVideo] = React.useState(false);
+
+const handleVideo = () =>{
+
+
+  setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+
+  if(playVideo){
+
+    videoRef.current.pause();
+  }else{
+
+    videoRef.current.play();
+  }
+}
+
+  return (
+    <div className="app__video">
+  
+      <video ref={videoRef} type="video/mp4" loop controls={false} muted src={meal}/>
+
+
+      <div className="app__video-overlay flex__center">
+
+        <div className="app__video-overlay_circle flex__center" onClick={handleVideo}>
+
+
+          {
+
+            playVideo ? <BsPauseFill color="#fff" fontSize={30}/> : <BsFillPlayFill  color="#fff" fontSize={30}/>
+          }
+
+
+        </div>
+
+
+      </div>
+     
+    </div>
+  );
+}
 
 export default Intro;
