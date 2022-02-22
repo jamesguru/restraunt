bb9543e953649d6cdbc4408ddb56a2d41a881a08
powerscript <jameskagunga2022@gmail.com>
2022-02-22 14:34:03 +0300

    finilizing project

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 
 import './Newsletter.css';
 
+import SubHeading from '../SubHeading/SubHeading'
+
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+
+    <div className="app__newsletter-heading">
+
+      <SubHeading title="Newsletter"/>
+
+      <h1 className="headtext__cormorant">Subscribe to our newsletter</h1>
+
+      <p className="p__opensans">And never miss latest news</p>
+
+
+    </div>
+
+
+    <div className="app__newsletter-input flex__center">
+
+      <input type="email" placeholder="Enter your email"/>
+
+
+      <button className="custom__button">Subscribe</button>
+
+
+    </div>
+    
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -2,9 +2,87 @@ import React from 'react';
 
 import './Footer.css';
 
+import {Newsletter,FooterOverlay} from '../../components';
+
+import {FiFacebook,FiInstagram,FiTwitter} from 'react-icons/fi';
+
+import {images} from '../../constants';
+
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+
+    <FooterOverlay />
+
+    <Newsletter />
+
+    <div className="app__footer-links">
+
+      <div className="app__footer-links_contact">
+
+        <h1 className="app__footer__headtext">Contact us</h1>
+
+
+        <p className="p__opensans">Dj Ruoro Public Address Services</p>
+
+        <p className="p__opensans">+254764568907</p>
+
+        <p className="p__opensans">+674521526383</p>
+
+
+      </div>
+
+      <div className="app__footer-links_logo">
+
+
+          <img src={images.gericht} alt="footer logo"/>
+
+          <p className="p__opensans">"The best way to find yourself is to lose yourself in service of others."</p>
+
+          <img src={images.spoon} alt="spoon" className="spoon__img" style={{marginTop: 15}} />
+
+          <div className="app__footer-links_icons">
+
+            <FiFacebook />
+
+            <FiTwitter />
+
+            <FiInstagram />
+
+
+
+
+          </div>
+
+      </div>
+
+      <div className="app__footer-links_work">
+
+
+      <h1 className="app__footer__headtext">Working Hours</h1>
+
+
+      <p className="p__opensans">Monday-Friday:</p>
+
+      <p className="p__opensans">10:00 Am - 02:00 Pm</p>
+
+      <p className="p__opensans">Sat-Sun</p>
+
+      <p className="p__opensans">10:00 Am - 03:00Pm </p>
+
+
+      </div>
+
+
+
+
+    </div>
+
+    <div className="footer__copyright">
+
+      <p className="p__opensans">2022 Gericht.All right reserved</p>
+
+
+    </div>
   </div>
 );
 
